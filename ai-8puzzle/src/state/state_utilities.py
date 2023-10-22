ea17d4c16af806091ad78083ccdf4f68b966f6c6
MariamAziz0 <ma27523@gmail.com>
2023-10-22 22:12:43 +0300

    Update state_utilities.py
    Adding utilities to help changing from one state to another

--- ai-8puzzle/src/state/state_utilities.py
+++ ai-8puzzle/src/state/state_utilities.py
@@ -1,7 +1,6 @@
 from typing import List
 
 
-# TODO
 def convert_1d_to_int(array_state: List[int]) -> int:
     """
     Converts the 1D game state and returns the integer game state.
@@ -12,14 +11,14 @@ def convert_1d_to_int(array_state: List[int]) -> int:
     Returns:
         int: An integer result.
     """
-    # Your implementation here
-
-    # Return the result as an integer
-    result = 0  # Replace with your computation
+    result = 0
+    for i in range(len(array_state) - 1, 0, -1):
+        result = result | array_state[i]
+        result = result << 4
+    result = result | array_state[0]
     return result
 
 
-# TODO
 def convert_int_to_1d(int_state: int) -> List[int]:
     """
     Converts the int game state and return the 1D game state.
@@ -30,13 +29,27 @@ def convert_int_to_1d(int_state: int) -> List[int]:
     Returns:
         List[int]: A 1D output array (list of integers).
     """
-    # Your implementation here
-
-    # Return the result as a list of integers
-    result = []  # Replace with your computation
+    result = []
+    while int_state != 0:
+        result = result + [int_state & 15]
+        int_state = int_state >> 4
     return result
 
 
-# TODO
-def move_zero(current_state: int, zero_current_position: int, zero_new_position: int) -> int:
-    return 0
+def move_zero(current_state: int, zeroPlace: int, anotherPlace: int) -> int:
+    """
+    Move zero from its place to another place, and
+    Move the another number to place of Zero.
+
+    Args:
+        current_state: int represent the value of current state
+        zeroPlace: the place of zero
+        anotherPlace: the place of another number
+
+    :returns
+         int: represent the value of new state
+    """
+    extractAnotherNum = ((15 << (anotherPlace * 4)) & (current_state)) >> (anotherPlace * 4)
+    current_state = current_state & (~(15 << (anotherPlace * 4)))
+    current_state = current_state | (extractAnotherNum << (zeroPlace * 4))
+    return current_state
