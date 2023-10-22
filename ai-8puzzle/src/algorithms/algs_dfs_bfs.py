e030e61a855198d8c2d8ca0ce1ec1af673c2e2d9
Maria Bassem <maria.bassem2002@gmail.com>
2023-10-23 01:19:26 +0300

    updating bfs and dfs

--- ai-8puzzle/src/algorithms/algs_dfs_bfs.py
+++ ai-8puzzle/src/algorithms/algs_dfs_bfs.py
@@ -1,9 +1,11 @@
-# from typing import List
+from typing import List
 
 from src.algorithms.frontier import Frontier
+from src.state.state import State
+from src.state.state_utilities import convert_1d_to_int, convert_int_to_1d
 
 
-def depth_first_search(start: int, goal: int) -> dict:
+def depth_first_search(start: State, goal: State) -> dict:
     # Implementation of DFS
     no_of_explored = 0  # initially nothing is explored
     frontier = Frontier("stack")
@@ -12,24 +14,30 @@ def depth_first_search(start: int, goal: int) -> dict:
     parent_map = {}
 
     frontier.push(start)
-    explored_and_frontier.add(start)
-    parent_map[start] = start
+    explored_and_frontier.add(start.value)
+    parent_map[start.value] = start.value
 
     while frontier.size() > 0:
-        curr = frontier.pop()
-        explored.add(curr)
+        curr: State = frontier.pop()
+        print("curr = ", convert_int_to_1d(curr.value))
+        explored.add(curr.value)
         no_of_explored += 1
 
-        if curr == goal:
+        if curr.value == g.value:
+        # if curr.is_goal():
             # we found the goal state
             print("DFS is Working ..")
             return parent_map
-        neighbors = curr.expand().reverse()
+        neighbors = curr.expand() # .reverse() gives error
+        # print("===================")
+        # for i in neighbors:
+        #     print("State: ", convert_int_to_1d(i.value))
+        # print("===================")
         for neighbor in neighbors:
-            if neighbor not in explored_and_frontier:
+            if neighbor.value not in explored_and_frontier:
                 frontier.push(neighbor)
-                explored_and_frontier.add(neighbor)
-                parent_map[neighbor] = curr
+                explored_and_frontier.add(neighbor.value)
+                parent_map[neighbor.value] = curr.value
     return False
     # print("DFS Working ..")
     # return [123456789]
@@ -37,31 +45,42 @@ def depth_first_search(start: int, goal: int) -> dict:
 
 def breadth_first_search(start: int, goal: int) -> bool:
     # Implementation of BFS
-
+    no_of_explored = 0  # initially nothing is explored
     frontier = Frontier("queue")
     explored = set()
     explored_and_frontier = set()
-    no_of_explored = 0  # initially nothing is explored
     parent_map = {}
 
     frontier.push(start)
-    explored_and_frontier.add(start)
-    parent_map[start] = start
+    explored_and_frontier.add(start.value)
+    parent_map[start.value] = start.value
 
     while frontier.size() > 0:
         curr = frontier.pop()
         explored.add(curr)
         no_of_explored += 1
 
-        if curr == goal:
+        if curr.value == goal.value:
             # we found the goal state
             print("BFS is Working ..")
             return parent_map
         for neighbor in curr.expand():
             if neighbor not in explored_and_frontier:
                 frontier.push(neighbor)
-                explored_and_frontier.add(neighbor)
-                parent_map[neighbor] = curr
+                explored_and_frontier.add(neighbor.value)
+                parent_map[neighbor.value] = curr.value
     return False
     # print("BFS Working ..")
     # return [123456789]
+
+array = [1,2,5,3,4,0,6,7,8]
+# print(array)
+a = convert_1d_to_int(array)
+s = State(a, 1, 1, 5)
+
+goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# print(goal)
+b = convert_1d_to_int(goal)
+g = State(b, 1, 1, 0)
+depth_first_search(s, g)
+print("finished")
