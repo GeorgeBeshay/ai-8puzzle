f0082023a9655acffda42ecd1860f9012ab23488
George Beshay <georgebeshay101@gmail.com>
2023-10-25 01:00:20 +0300

    Updated algs_factory.py

--- ai-8puzzle/src/algorithms/algs_factory.py
+++ ai-8puzzle/src/algorithms/algs_factory.py
@@ -10,6 +10,7 @@ def algorithm_factory(algorithm_type: str) -> Callable[[State, int], Solution]:
     elif algorithm_type == "BFS":
         return breadth_first_search
     elif algorithm_type == "A*":
-        return a_star
+        heuristic = "Manhattan" if input("Enter heuristic function (M / E): ").strip().upper() == "M" else "Euclidean"
+        return lambda int_state: a_star(int_state, heuristic=heuristic)
     else:
         raise ValueError("Invalid algorithm type")
